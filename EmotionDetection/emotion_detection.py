fa6df9e2f61bd297484ed6d65e30b1581408ea8b
mickos939 <222618871+mickos939@users.noreply.github.com>
2026-04-19 17:21:54 -0400

    Packaged application

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -15,5 +15,17 @@ def emotion_detector(text_to_analyze):
     }
 
     response = requests.post(url, headers=headers, json=input_json)
+    response_dict = json.loads(response.text)
+    emotions = response_dict["emotionPredictions"][0]["emotion"]
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
     
-    return response.text
